458e65e4baadf15139c57348178802b8236f1750
Krystsina <krystsina.kurytsyna@daftmobile.com>
2023-05-23 13:45:58 +0200

    add main template

--- main.py
+++ main.py
@@ -1,26 +1,41 @@
-import random
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from telegram import Bot
 
-def generate_linux_command():
-    commands = [
-        'ls',
-        'pwd',
-        'cat file.txt',
-        'grep "pattern" file.txt',
-        'chmod 755 script.sh',
-        'mkdir directory',
-        'rm file.txt',
-        'tar -czvf archive.tar.gz directory',
-        'find . -name "*.txt"',
-        'ssh user@hostname',
-        'sudo apt-get update',
-        'top',
-        # Add more commands as desired
-    ]
-    return random.choice(commands)
+# Set up Telegram bot
+telegram_bot_token = 'YOUR_TELEGRAM_BOT_TOKEN'
+bot = Bot(token=telegram_bot_token)
 
-def main():
-    random_command = generate_linux_command()
-    print("Random Linux Command:", random_command)
+# Set up Selenium
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
+driver = webdriver.Chrome(options=chrome_options)
 
-if __name__ == '__main__':
-    main()
+# Scraping function
+def scrape_website():
+    driver.get('https://example.com')  # Replace with your university website URL
+    # Perform scraping and extract necessary information
+    # ...
+
+# Compare function
+def compare_data(new_data):
+    # Compare new data with previously stored data
+    # ...
+
+# Main loop
+while True:
+    # Scrape the website
+    new_data = scrape_website()
+
+    # Compare data to detect changes
+    if compare_data(new_data):
+        # Send notification
+        bot.send_message(chat_id='YOUR_CHAT_ID', text='Changes detected!')
+
+    # Wait for an hour before scraping again
+    time.sleep(3600)
